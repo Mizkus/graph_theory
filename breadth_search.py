f3754ecea58040fa9fce14cce498198d417cddf9
Mizkus <56596273+Mizkus@users.noreply.github.com>
2021-04-18 14:14:45 +0800

    release

--- breadth_search.py
+++ breadth_search.py
@@ -1,30 +1,66 @@
+def index_convert_graph(graph, start_node = None, end_node = None):
+    key_index = dict()
+    value_index = dict()
+    c = 0
+    for k in graph.keys():
+        key_index[k] = c
+        value_index[c] = k
+        c += 1
+    if start_node != None: start_node = key_index[start_node]
+    if end_node != None: end_node = key_index[end_node]
+    return (key_index, start_node, end_node, value_index)
+
+def graph_converter(graph):
+    graph_index = dict()
+    for i in range(len(graph)):
+        graph_index[i] = {}
+    for i in graph.keys():
+        for j in graph[i]:
+            temp1 = index_convert_graph(graph)[0][i]
+            temp2 = index_convert_graph(graph)[0][j]
+            graph_index[temp1] = [temp2]
+    return graph_index
+
 def breadth_search(graph, start_node, end_node):
     from collections import deque
-    start_vertex = start_node
-    end_vertex = end_node
+    graph_index = graph_converter(graph)
+    start_node = index_convert_graph(graph, start_node)[1]
+    end_node = index_convert_graph(graph, end_node = end_node)[2]
     visited = [start_node]
     queue = deque([start_node])
-    parents = [None] * len(graph)
+    parents = [None] * len(graph_index)
     while queue:
         node = queue.popleft()
-        for neighbour_node in graph[node]:
+        for neighbour_node in graph_index[node]:
             if neighbour_node not in visited:
                 visited.append(neighbour_node)
-                parents[neighbour_node] = node
+                parents[neighbour_node] = index_convert_graph(graph)[3][node]
                 queue.append(neighbour_node)
-    path = [end_node]
+    path = [index_convert_graph(graph)[3][end_node]]
     parent = parents[end_node]
     while not parent is None:
         path.append(parent)
-        parent = parents[parent]
-    return len(path[::-1]) - 1
+        parent = parents[index_convert_graph(graph)[0][parent]]
+    return (len(path) - 1, path[::-1])
 
-graph = {
-    0: [1, 2],
-    1: [3],
-    2: [4],
-    3: [4],
-    4: [5],
-    5: []
-}
-print("Наименьшее необходимое количество шагов: ", breadth_search(graph, 0, 5))
+if __name__ == "__main__":
+
+    graph = {
+        "A": ["E", "C"],
+        "C": ["D"],
+        "D": ["B"],
+        "E": ["F"],
+        "F": ["D"],
+        "B": []
+    }
+    # TODO: раскоментировать если нужно сделать выбор
+    #start_node, end_node = map(str, input("Введите начальный и конечный узел, через пробел: ").split())
+
+    #TODO: закоментировать если нужно сделать выбор
+    start_node = "A"
+    end_node = "B"
+
+    breadth_search_output = breadth_search(graph, start_node, end_node)
+
+    print("Наименьшее необходимое количество шагов: ", breadth_search_output[0])
+    print(*breadth_search_output[1], sep = "->")
--- dijkstra.py
+++ dijkstra.py
@@ -1,24 +1,42 @@
-graph = {
-    0: {1: [6], 2: [2]},
-    1: {3: [1]},
-    2: {1: [3], 3: [5]},
-    3: {}
-}
+def index_convert_graph(graph, start_node = None, end_node = None):
+    key_index = dict()
+    value_index = dict()
+    c = 0
+    for k in graph.keys():
+        key_index[k] = c
+        value_index[c] = k
+        c += 1
+    if start_node != None: start_node = key_index[start_node]
+    if end_node != None: end_node = key_index[end_node]
+    return (key_index, start_node, end_node, value_index)
 
-
+def graph_converter(graph):
+    graph_index = dict()
+    for i in range(len(graph)):
+        graph_index[i] = {}
+    for i in graph.keys():
+        for j, l in graph[i].items():
+            temp1 = index_convert_graph(graph)[0][i]
+            temp2 = index_convert_graph(graph)[0][j]
+            graph_index[temp1][temp2] = l
+    return graph_index
 
 def make_matrix(graph):
+    graph = graph_converter(graph)
     matrix = [0] * len(graph)
     for i in graph.keys():
         matrix[i] = [float("inf")] * len(graph)
-    for i in graph.keys():
+    for i in range(len(graph)):
         for j in graph[i].keys():
             matrix[i][j] = graph[i][j][0]
     return matrix
 
 def dijkstra(graph, start_node, end_node):
+    start_node = index_convert_graph(graph, start_node)[1]
+    end_node = index_convert_graph(graph, end_node=end_node)[2]
     matrix = make_matrix(graph)
     amount_nodes = len(matrix)
+    parents = [None] * amount_nodes
     not_visited = [True]*amount_nodes
     costs = [float("inf")]*amount_nodes
     costs[start_node] = 0
@@ -30,11 +48,37 @@ def dijkstra(graph, start_node, end_node):
                 min_cost = costs[j]
                 min_cost_index = j 
         for k in range(amount_nodes):
-            print(costs[k])
             if costs[min_cost_index] + matrix[min_cost_index][k] < costs[k]:
                 costs[k] = costs[min_cost_index] + matrix[min_cost_index][k]
+                parents[k] = index_convert_graph(graph)[3][min_cost_index]
         not_visited[min_cost_index] = False
-    return costs[end_node]
+    path = [index_convert_graph(graph)[3][end_node]]
+    parent = parents[end_node]
+    while not parent is None:
+        path.append(parent)
+        parent = parents[index_convert_graph(graph)[0][parent]]
+    return (costs[end_node], path[::-1])
 
-print("Наименьшее время:", dijkstra(graph, 0, 3), "минут")
+if __name__ == "__main__":
+
+    graph = {
+        "A": {"C": [6], "D": [2]},
+        "C": {"B": [1]},
+        "D": {"C": [3], "B": [5]},
+        "B": {}
+    }
+
+    # TODO: раскоментировать если нужно сделать выбор
+    #start_node, end_node = map(str, input("Введите начальный и конечный узел, через пробел: ").split())
+
+    #TODO: закоментировать если нужно сделать выбор
+    start_node = "A"
+    end_node = "B"
+
+    dijkstra_output = dijkstra(graph, start_node, end_node)
+    print("Минимальное время: ", dijkstra_output[0], "минут")
+    print(*dijkstra_output[1], sep = "->")
+
+
+
 
